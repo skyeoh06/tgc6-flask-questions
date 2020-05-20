0f055d875758fc49b6f6dcf18fe5286a3d1296f5
kunxin-chor <chorkunxin@yahoo.com>
2020-05-20 12:43:24 +0000

    Added some solutons and bug fixes

--- 04-basic-templates/app.py
+++ 04-basic-templates/app.py
@@ -3,6 +3,17 @@ import os
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.template.html')
+
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.template.html')
+
+
+
 # "magic code" -- boilerplate
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
