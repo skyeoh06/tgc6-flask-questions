85e6f2894c3696a1e93650218eba613625303714
kunxin-chor <chorkunxin@yahoo.com>
2020-05-28 12:42:53 +0000

    Added U of CRUD

--- 08-crud/app.py
+++ 08-crud/app.py
@@ -73,13 +73,66 @@ def display_add_form():
 def process_add():
 
     # step 1 - get the data
-    print(request.form)
+    isbn = request.form.get('isbn')
+    title = request.form.get('book-title')
+    author = request.form.get('author')
 
     # step 2 - process the data
 
-    # step 3 - render a template or redirect to a view function
+    # step 2.1 : open the file
+    with open('books.csv', 'a', newline="\n") as fp:
+        # step 2.2: add in new row in
 
-    return "data recieved"
+        # step 2.3 create the csv writer
+        writer = csv.writer(fp, delimiter=",")
+
+        # step 2.4 append the row in using the csv writer
+        writer.writerow([isbn, title, author])
+
+    # step 3 - render a template or redirect to a view function
+    return redirect(url_for('show_books'))
+
+
+@app.route('/edit_book/<isbn>')
+def edit_book(isbn):
+    # step 1: get the book specified by the isbn
+    book = None
+    all_books = get_all_books_from_file()
+    for b in all_books:
+        if b['isbn'] == isbn:
+            book = b
+            break
+
+    return render_template('edit_book.template.html', book=book)
+
+
+@app.route('/edit_book/<isbn>', methods=["POST"])
+def process_edit_book(isbn):
+    # step 1: read in all the books
+    books = get_all_books_from_file()
+
+    # step 2: find the book to be updated by its isbn
+    for i in range(len(books)):
+        # step 3: update the book in the list directly
+        if books[i]['isbn'] == isbn:
+            books[i]['isbn'] = request.form.get('isbn')
+            books[i]['title'] = request.form.get('book-title')
+            books[i]['author'] = request.form.get('author')
+            break
+
+    # step 4: write all books into data.csv
+    # overwriting the csv file with the modified book information
+    with open('books.csv', 'w', newline="\n") as fp:
+        writer = csv.writer(fp, delimiter=",")
+        writer.writerow(['isbn', 'title', 'author'])
+        for b in books:
+            writer.writerow([
+                b['isbn'],
+                b['title'],
+                b['author']
+            ])
+
+    return redirect(url_for('show_books'))
 
 
 # "magic code" -- boilerplate
