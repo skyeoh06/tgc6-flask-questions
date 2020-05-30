43451726be35f7c6d04372f243fa299dd53ffe68
skyeoh06 <felyciayeoh@yahoo.com>
2020-05-30 11:24:02 +0000

    02-answer

--- 02-basic-routing/main.py
+++ 02-basic-routing/main.py
@@ -4,6 +4,21 @@ import os
 app = Flask(__name__)
 
 # your code here
+@app.route('/')
+def home():
+    return "hi everybody!"
+
+@app.route('/about')
+def about_me():
+    return "i am very smart"
+
+@app.route('/double/<number>')
+def double(number):
+    return str(int(number) * 2)
+
+@app.route('/add/<n1>/<n2>')
+def add(n1, n2):
+    return str(int(n1)+int(n2))
 
 # "magic code" -- boilerplate
 if __name__ == '__main__':
