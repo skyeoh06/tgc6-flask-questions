da23a673d04a0da1b1685702967f9a722bf75ee2
skyeoh06 <felyciayeoh@yahoo.com>
2020-05-30 14:08:28 +0000

    05-answer

---
+++ 05-more-templates/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.template.html')
+
+@app.route('/products')
+def products():
+    return render_template('products.template.html')
+# "magic code" -- boilerplate
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
